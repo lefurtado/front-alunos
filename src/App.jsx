c6e3d769356ebda7df93e84d7ef90d613a9f518c
Leandro Furtado <le_furtado@outlook.com.br>
2024-02-02 18:31:19 -0300

    add: atualização das funções de chamada da api utilizando o react-query, atualização em tempo real na aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,115 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import api from "./services/api.js";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 function App() {
-  const [alunos, setAlunos] = useState([]);
+  // criado instancia do queryClient para utilização posterior
+  const queryClient = useQueryClient();
+  // state que segura o estado dos inputs
   const [input, setInput] = useState({
     nome: "",
     cpf: "",
   });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await api.get();
-        setAlunos(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
+  // função que faz a chamada na API para trazer os dados dos alunos
+  // que existem no bando de dados
+  const fetchData = async () => {
+    try {
+      // metodo que faz com que a resposta retorne os dados da API
+      const res = await api.get();
+      return res.data;
+    } catch (error) {
+      throw new Error("Erro ao carregar dados do servidor!");
+    }
+  };
 
+  // função que faz a chamada na API para criação de um aluno no banco de dados
+  const createAluno = async () => {
+    try {
+      // método post para criação do aluno no DB
+      const res = await api.post("/", input);
+      return res.data;
+    } catch (error) {
+      throw new Error("Erro ao carregar dados do servidor!");
+    }
+  };
+
+  // função que faz a chamada na API para exclusão de um aluno e recebe
+  // como parâmetro o id desse aluno
+  const deleteAluno = async (id) => {
+    try {
+      // executa o método delete do axio mandando pra URL o ID do aluno
+      // que está sendo recebido como parâmetro da função
+      const res = await api.delete(`/${id}`);
+      return res.data;
+    } catch (err) {
+      alert("Erro ao deletar aluno!");
+    }
+  };
+
+  // função que lida com o submit do formulário para criação de um aluno, recebe
+  // data como parametro
+  async function handleSubmit(data) {
+    try {
+      await createAlunoFn({
+        name: data.nome,
+        cpf: data.cpf,
+      });
+    } catch (err) {
+      alert("Erro ao cadastrar aluno!");
+    }
+  }
+
+  // função que lida com o delete do aluno que também recebe data como parametro
+  async function handleDelete(data) {
+    try {
+      await deleteAlunoFn(data);
+    } catch (err) {
+      alert("Erro ao deletar aluno!");
+    }
+  }
+
+  // chamada do useQuery para mostrar os dados em
+  // tela sem precisar usar useState, utilizando a "queryKey"
+  // é possível armazenar um estado HTTP que ficará escutando essa key
+  // por toda a aplicação
+  const { data: alunos, isLoading } = useQuery({
+    queryKey: ["alunos"],
+    // a "queryFn" é utilizada pra fazer a chamada da função que fará o
+    // fetch dos dados que serão mostrados
+    queryFn: fetchData,
+  });
+
+  // o hook useMutation serve para criação, alteração e exclusão de algum
+  // item no banco de dados, essa lista que vem do banco de dados
+  // irá alterar e será necessário fazer alteração em tempo real
+  // na tela para mostrar a tabela atualizada
+  const { mutateAsync: createAlunoFn } = useMutation({
+    mutationFn: createAluno,
+  });
+
+  // o uso do useMutation aqui vem em conjunto com a função "onSuccess"
+  // que serve pra atualizar a query assim que o aluno for deletado do db,
+  // ela escuta a queryKey criada pelo useQuery e reexecuta uma chamada
+  // fetch para mostrar os dados atualizados apenas quando a função
+  // deleteAluno for executada
+  const { mutateAsync: deleteAlunoFn } = useMutation({
+    mutationFn: deleteAluno,
+    onSuccess() {
+      queryClient.invalidateQueries(["alunos"]);
+    },
+  });
+
+  // caso o isLoading seja true, ou seja, ainda esteja carregando os dados
+  // dos alunos, uma div com o conteúdo carregando é mostrado ao usuário
+  if (isLoading) {
+    return <div>Carregando...</div>;
+  }
+
+  // trata o input
   const handleInput = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
   };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const res = await api.post("/", input);
-      console.log("Novo aluno adicionado: ", res.data);
-      setAlunos(state => [...state, res.data]);
-      setInput({ nome: "", cpf: "" });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleDelete = async (id) => {
-    try {
-      await api.delete(`/${id}`);
-      setAlunos(alunos.filter((aluno) => aluno._id !== id));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  if (!alunos) return <div>Loading...</div>;
 
   return (
     <div className="flex items-center justify-center h-screen text-center">
@@ -75,7 +139,7 @@ function App() {
           </div>
         </form>
         <div className="flex gap-4">
-          {alunos.map((aluno) => (
+          {alunos?.map((aluno) => (
             <ul key={aluno._id}>
               <li>Nome: {aluno.nome}</li>
               <li>CPF: {aluno.cpf}</li>
